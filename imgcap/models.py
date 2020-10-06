e9c4076e048dfddf5aea74ae963769252b168d97
nunenuh <nunenuh@gmail.com>
2020-10-06 20:15:28 +0800

    fix: change model to working encoder and decoder model with pack_padded_sequence

--- imgcap/models.py
+++ imgcap/models.py
@@ -1,70 +1,79 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torch.nn.utils.rnn import pack_padded_sequence
 
-
-class EncoderCNN(nn.Module):
-    def __init__(self, embed_size, train_CNN=False):
-        super(EncoderCNN, self).__init__()
-        self.train_CNN = train_CNN
-        self.resnet = models.resnet34(pretrained=True)
-        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, embed_size)
-        self.relu = nn.ReLU()
-        self.times = []
+class Encoder(nn.Module):
+    def __init__(self, embed_size, train_cnn=False, resnet_model=models.resnet50):
+        super(Encoder, self).__init__()
+        self.train_cnn = train_cnn
+        resnet = resnet_model(pretrained=True)
+        resnet = self._fine_tune(resnet)
+        modules = list(resnet.children())[:-1]
+        
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
         self.dropout = nn.Dropout(0.5)
-
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+        
+    def _fine_tune(self, resnet):
+        if self.train_cnn:
+            for param in resnet.parameters():
+                param.requires_grad_(True)
+        else:
+            for param in resnet.parameters():
+                param.requires_grad_(False)
+                
+        return resnet
+    
     def forward(self, images):
         features = self.resnet(images)
-        return self.dropout(self.relu(features))
+        features = features.view(features.size(0), -1)
+        embed = self.bn(self.embed(features))
+        return embed
 
 
-class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers, dropout_p=0.5):
-        super(DecoderRNN, self).__init__()
+class Decoder(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(Decoder, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        
         self.embed = nn.Embedding(vocab_size, embed_size)
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, dropout=dropout_p, 
-                            batch_first=True, bidirectional=True)
-        self.linear = nn.Linear(hidden_size * 2, vocab_size)
-        self.dropout = nn.Dropout(0.5)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        
+        self._init_weights()
+                
+    def _init_weights(self):
+        torch.nn.init.xavier_uniform_(self.linear.weight)
+        torch.nn.init.xavier_uniform_(self.embed.weight)
 
-    def forward(self, features, captions):
-        captions = captions[:, :-1]
-        embeddings = self.dropout(self.embed(captions))
-        embeddings = torch.cat((features.unsqueeze(1), embeddings), dim=1)
-        hiddens, _ = self.lstm(embeddings)
-        outputs = self.linear(hiddens)
+    def forward(self, features, captions, lengths):
+        features = features.unsqueeze(dim=1)
+        
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features, embeddings), dim=1)
+        packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
+        hiddens, _ = self.lstm(packed)
+        outputs = self.linear(hiddens[0])
         return outputs
-
+    
 
 class ImageCaptionNet(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
         super(ImageCaptionNet, self).__init__()
-        self.encoderCNN = EncoderCNN(embed_size)
-        self.decoderRNN = DecoderRNN(embed_size, hidden_size, vocab_size, num_layers)
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        
+        self.encoder = Encoder(embed_size)
+        self.decoder = Decoder(embed_size, hidden_size, vocab_size, num_layers)
 
-    def forward(self, images, captions):
-        features = self.encoderCNN(images)
-        outputs = self.decoderRNN(features, captions)
+    def forward(self, images, captions, lengths):
+        features = self.encoder(images)
+        outputs = self.decoder(features, captions, lengths)
         return outputs
-
-    def caption_image(self, image, vocabulary, max_length=50):
-        result_caption = []
-
-        with torch.no_grad():
-            x = self.encoderCNN(image).unsqueeze(0)
-            states = None
-
-            for _ in range(max_length):
-                hiddens, states = self.decoderRNN.lstm(x, states)
-                output = self.decoderRNN.linear(hiddens.squeeze(0))
-                predicted = output.argmax(1)
-                result_caption.append(predicted.item())
-                x = self.decoderRNN.embed(predicted).unsqueeze(0)
-
-                if vocabulary.itos[predicted.item()] == "<EOS>":
-                    break
-
-        return [vocabulary.itos[idx] for idx in result_caption]
-
-    
-    
